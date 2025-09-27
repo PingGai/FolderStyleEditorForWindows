65cc8cdf30b38b7e770310c47e7aaa3ebb6b4508
PING <3036736554@qq.com>
2025-09-28 06:07:13 +0800

    Initial commit with .gitignore and project files

---
+++ WindowsFolderStyleEditor/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace WindowsFolderStyleEditor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
